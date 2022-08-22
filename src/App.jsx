3ec187441d5985d346d4647d0f88b35efa3ddc75
Benz Tan <55311935+bwjctan1999@users.noreply.github.com>
2022-08-22 15:18:31 +0800

    added routing and home routh path

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import { useState } from 'react'
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import Home from './pages/Home';
 
 function App() {
   return (
     <div>
-      
+        <BrowserRouter>
+          <Routes>
+            <Route path="/*" element={<Home />}/>
+          </Routes>
+        </BrowserRouter>
     </div>
   )
 }
