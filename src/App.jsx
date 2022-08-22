cea4e3f429c1e1f246af5504cf43054a98108319
Benz Tan <55311935+bwjctan1999@users.noreply.github.com>
2022-08-23 07:53:43 +0800

    added Route to storepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import { useState } from 'react'
+import { useState } from "react";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
-import Home from './pages/Home';
+import Home from "./pages/Home";
+import Store from "./pages/Store";
 
 function App() {
   return (
     <div>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/*" element={<Home />}/>
-          </Routes>
-        </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/*" element={<Home />} />
+          <Route path="/Store" element={<Store />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
