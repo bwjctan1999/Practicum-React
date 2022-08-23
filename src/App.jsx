618046954e36c33d168b59a88b900b892b28ce83
Benz Tan <55311935+bwjctan1999@users.noreply.github.com>
2022-08-23 08:53:22 +0800

    added Routing for signup

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Home from './pages/Home';
+import Signup from './pages/Signup';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
         <BrowserRouter>
           <Routes>
             <Route path="/*" element={<Home />}/>
+            <Route path="/Signup" element={<Signup />} />
           </Routes>
         </BrowserRouter>
     </div>
