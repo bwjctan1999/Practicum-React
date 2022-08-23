6a27f403b24e6835809d3109e500a80df5e75bfe
Benz Tan <55311935+bwjctan1999@users.noreply.github.com>
2022-08-23 08:20:46 +0800

    added TextField component

---
+++ src/components/TextField.jsx
@@ -0,0 +1,9 @@
+export default function TextField({ type, placeholder }) {
+  return (
+    <input
+      type={type}
+      placeholder={placeholder}
+      className="p-3 text-sm bg-white w-full border border-opacity-50 rounded-2xl shadow text-black outline-none"
+    />
+  );
+}
